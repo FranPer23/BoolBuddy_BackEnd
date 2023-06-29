379696a6b85d0ff612adedd53175d5a27e6af530
FranPer23 <francescaprt@yahoo.com>
2023-06-29 17:41:28 +0200

    first commit

---
+++ database/migrations/2023_06_29_152448_create_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->text('photo')->nullable();
+            $table->string('mobile')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->string('cv')->nullable();
+            $table->string('slug')->unique();
+            $table->string('field');
+            $table->string('service');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
