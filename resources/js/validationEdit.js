bfa3009159cc791a1c3ce0a427819c3a792585c0
FranPer23 <francescaprt@yahoo.com>
2023-07-06 17:12:59 +0200

    fix validation edit page

--- resources/js/validationEdit.js
+++ resources/js/validationEdit.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.ms_form');
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const password = document.getElementById('password');
-    const passwordConfirm = document.getElementById('password-confirm');
+
     const errorMessage = document.getElementById('errorTech');
-    const errorMessagePw = document.getElementById('errorPw');
-    const errorMessagePwConfirm = document.getElementById('errorPw2');
+
     const name = document.getElementById('name');
     const surname = document.getElementById('surname');
     const eMail = document.getElementById('email');
@@ -13,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const city = document.getElementById('city');
     const errorNameMessageEmpty = document.getElementById('errorNameEmpty');
     const errorSurnameMessageEmpty = document.getElementById('errorSurnameEmpty');
-    const errorEmailMessageEmpty = document.getElementById('errorEmailEmpty');
+
     const errorAddressMessageEmpty = document.getElementById('errorAddressEmpty');
     const errorCityMessageEmpty = document.getElementById('errorCityEmpty');
-    const errorMessageMailFormat = document.getElementById('errorMailFormat');
+
     
 
     form.addEventListener('submit', function(event) 
@@ -30,16 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             errorMessage.textContent ='Select at least one technology' ;
         }
-        if (password.value.length < 8) {
-                event.preventDefault();
-                errorMessagePw.textContent ='Passwords must contain at least 8 characters';
-            } 
-            
-        if(password.value !== passwordConfirm.value ) {
-                event.preventDefault();
-                errorMessagePwConfirm.textContent ='Passwords does not match';
-
-             } 
+      
 
         if(name.value === '' ) {
                 event.preventDefault();
@@ -65,19 +54,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
              } 
         
-        if(eMail.value === '' ) {
-                event.preventDefault();
-                errorEmailMessageEmpty.textContent ='Field must not be empty';
-
-             } 
-
-        if(!eMail.value.includes('@') ) {
-                event.preventDefault();
-                errorMessageMailFormat.textContent ='Field must contain a valid e-mail address (ex.: example@example.com)';
-
-             }
-             
-       
    
     });
 });
--- resources/views/admin/profiles/edit.blade.php
+++ resources/views/admin/profiles/edit.blade.php
@@ -25,36 +25,44 @@
                             @csrf
 
                             <div class="mb-3">
-                                <label for="name" class="form-label">Name*</label>
+                                <label for="name" class="form-label">Name*</label><br> <span class="text text-danger"
+                                    id="errorNameEmpty"></span>
                                 <input type="text" class="form-control" id="name" name="name"
-                                    value="{{ old('name', $profile->name) }}">
+                                    value="{{ old('name', $profile->name) }}" autofocus>
 
                             </div>
-                            <span class="text text-danger" id="errorNameEmpty"></span>
+
 
                             <div class="mb-3">
-                                <label for="surname" class="form-label">Surname*</label>
+                                <label for="surname" class="form-label">Surname*</label> <br><span class="text text-danger"
+                                    id="errorSurnameEmpty"></span>
                                 <input type="text" class="form-control" id="surname" name="surname"
-                                    value="{{ old('surname', $profile->surname) }}">
+                                    value="{{ old('surname', $profile->surname) }}" autofocus>
                             </div>
 
+
                             <div class="mb-3">
-                                <label for="address" class="form-label">Address*</label>
+                                <label for="address" class="form-label">Address*</label><br><span class="text text-danger"
+                                    id="errorAddressEmpty"></span>
                                 <input type="text" class="form-control" id="address" name="address"
-                                    value="{{ old('address', $profile->address) }}">
+                                    value="{{ old('address', $profile->address) }}" autofocus>
                             </div>
 
+
                             <div class="mb-3">
-                                <label for="city" class="form-label">City*</label>
+                                <label for="city" class="form-label">City*</label><br><span class="text text-danger"
+                                    id="errorCityEmpty"></span>
                                 <input type="text" class="form-control" id="city" name="city"
-                                    value="{{ old('city', $profile->city) }}">
+                                    value="{{ old('city', $profile->city) }}" autofocus>
                             </div>
+
 
                             <div class="mb-3">
                                 <label for="photo" class="form-label">Photo</label>
                                 <input type="file" class="form-control" id="photo" name="photo"
                                     value="{{ old('photo', $profile->photo) }}">
                             </div>
+
 
                             <div class="mb-3">
                                 <label for="phone" class="form-label">Phone</label>
@@ -87,14 +95,15 @@
                             </div>
 
                             <div class="form-check">
-                                <label for="service" class="form-label">Technologies*</label>
+                                <label for="service" class="form-label">Technologies*</label><br><span
+                                    class="text text-danger" id="errorMessage"></span>
                                 @foreach ($technologies as $technology)
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" value="{{ $technology->id }}"
                                             id="technology{{ $technology->id }}" name="technologies[]"
                                             @checked(old('technologies')
                                                     ? in_array($technology->id, old('technologies', []))
-                                                    : $profile->technology->contains($technology))>
+                                                    : $profile->technology->contains($technology)) autofocus>
                                         <label class="form-check-label" for="technology{{ $technology->id }}">
                                             {{ $technology->name }}
                                         </label>
