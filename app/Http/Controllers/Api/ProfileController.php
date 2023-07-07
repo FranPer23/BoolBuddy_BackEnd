593f7722783925ab81e2bbb7156a4fcec1678b07
FranPer23 <francescaprt@yahoo.com>
2023-07-07 11:15:42 +0200

    test api

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        $profiles = Profile::all();
+
+        return response()->json([
+            'success' => true, // non è obbligatorio, mi serve solo per dire che la chiamata è avvenuta con successo
+            'results' => $profiles
+        ]);
+    }
+
+    public function show($id)
+    {
+        //metto first perchè non voglio tutti gli elementi, ma solo il primo
+        $profile = Profile::with('technologies')->where('id', $id)->first();
+
+
+        //se il progetto esiste mi da success true altrimenti mi restituisce false 
+        if ($profile) {
+            return response()->json([
+                'success' => true,
+                'results' => $profile
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun profilo trovato'
+            ])->setStatusCode(404); //mi restituisce errore 404 perchè la pagina non è stata trovata
+        }
+    }
+}
